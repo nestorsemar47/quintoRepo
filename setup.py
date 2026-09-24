730001b97cc911ae8fa9873a96e3ba6375ab72c1
NestorMar <nestorsemar47>
2026-09-23 21:47:48 -0600

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quintoRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Nestor Martinez",                         # Tu nombre
+    author_email="nestorsemar47@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/nestorsemar47/quintoRepo",     # URL del proyecto
+)
